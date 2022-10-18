acea3ad261f599fb6f5ad591d73468735e0012cd
shakilkhan12 <shakilkhan621@gmail.com>
2022-10-18 19:26:07 +0500

    fetch data from jsonplaceholder api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,52 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 const Home = () => {
-  const [count, setCount] = useState(0);
-  const [state, setState] = useState(true);
-  useEffect(
-    function () {
-      console.log("Run useEffect");
-    },
-    [count, state]
-  );
+  const [users, setUsers] = useState([]);
+  const [loader, setLoader] = useState(false);
+  useEffect(function () {
+    setLoader(true);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function (yourData) {
+        setLoader(false);
+        setUsers(yourData.data);
+      })
+      .catch(function (error) {
+        setLoader(false);
+        console.log(error);
+      });
+  }, []);
+  console.log("last line");
   return (
     <div className="max-w-screen-lg mx-auto">
-      {console.log("Run JSX")}
-      <h1 className="mt-2 text-[40px] font-medium">Home page</h1>
-      <button
-        className="bg-rose-600 rounded px-4 py-3 text-white capitalize"
-        onClick={() => setCount(count + 1)}
-      >
-        Inc {count}
-      </button>
-      <button
-        onClick={() => setState(!state)}
-        className="bg-green-600 rounded px-4 py-3 text-white capitalize"
-      >
-        {state ? "True" : "False"}
-      </button>
+      {loader ? (
+        "Loading..."
+      ) : users.length > 0 ? (
+        <table className="w-full my-10">
+          <thead>
+            <tr>
+              <th className="border p-2 text-left uppercase">name</th>
+              <th className="border p-2 text-left uppercase">username</th>
+              <th className="border p-2 text-left uppercase">email</th>
+              <th className="border p-2 text-left uppercase">website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr className="odd:bg-indigo-50">
+                  <td className="border p-4">{user.name}</td>
+                  <td className="border p-4">{user.username}</td>
+                  <td className="border p-4">{user.email}</td>
+                  <td className="border p-4">{user.website}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        "No users"
+      )}
     </div>
   );
 };
